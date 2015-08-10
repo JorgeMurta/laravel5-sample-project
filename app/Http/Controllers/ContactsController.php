ee34d2a27411bf027f43d75959a3c198c38ebe07
Jorge Murta <jmurta.jm@gmail.com>
2015-08-10 18:21:41 +0100

    Organize Jobs Folder

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -52,7 +52,7 @@ class ContactsController extends Controller
      */
     public function postCreate(CreateContactRequest $request)
     {
-        $this->dispatchFrom(\App\Jobs\CreateContactJob::class, $request);
+        $this->dispatchFrom(\App\Jobs\Contact\CreateContactJob::class, $request);
     	return redirect()->action('ContactsController@getIndex');
     }
 
@@ -77,7 +77,7 @@ class ContactsController extends Controller
     public function postUpdate($id, EditContactRequest $request)
     {
         $request["id"] = $id;
-        $this->dispatchFrom(\App\Jobs\UpdateContactJob::class, $request);
+        $this->dispatchFrom(\App\Jobs\Contact\UpdateContactJob::class, $request);
     	return redirect()->action('ContactsController@getIndex');
     }
 
@@ -88,7 +88,7 @@ class ContactsController extends Controller
      */
     public function getDelete($id)
     {
-        $this->dispatch(new \App\Jobs\DeleteContactJob($id));
+        $this->dispatch(new \App\Jobs\Contact\DeleteContactJob($id));
     	return redirect()->action('ContactsController@getIndex');
     }
 }
--- app/Jobs/Contact/DeleteContactJob.php
+++ app/Jobs/Contact/DeleteContactJob.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Jobs;
+namespace App\Contact\Jobs;
 
 use App\Jobs\Job;
 use Illuminate\Contracts\Bus\SelfHandling;
